82c88e1fb5a36a7df42a359d288eefd02498c597
Claudia <claudiaareneee@gmail.com>
2020-04-20 21:20:56 -0500

    added a plot for knn, but it doesn't count all of the variables

--- knn.py
+++ knn.py
@@ -1,7 +1,9 @@
 # pylint: disable=maybe-no-member
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
-import graphviz 
 
 iris = datasets.load_iris()
 
@@ -10,5 +12,34 @@ print(iris.target)
 print(iris.target_names)
 
 model = KNeighborsClassifier(n_neighbors=3)
-model = model.fit(iris.data, iris.target)
+# model = model.fit(iris.data, iris.target)
+X = iris.data[:, :2];
+y = iris.target;
 
+model = model.fit(X, y)
+
+h = .02  # step size in the mesh
+
+# Create color maps
+cmap_light = ListedColormap(['orange', 'cyan', 'cornflowerblue'])
+cmap_bold = ListedColormap(['darkorange', 'c', 'darkblue'])
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, x_max]x[y_min, y_max].
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure()
+plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.title("3-Class classification (k = %i, weights = '%s')" % (3, 0))
+
+plt.show()
