2d4ca6fc726a0cd5e83353db4fa1deb80539a864
Claudia <claudiaareneee@gmail.com>
2020-04-20 22:03:55 -0500

    restructured knn

--- Part A/knn.py
+++ Part A/knn.py
@@ -3,44 +3,43 @@
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
-from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
 
-iris = datasets.load_iris()
+def kNearestNeighbors(iris, k):
+    model = KNeighborsClassifier(n_neighbors=k)
+    # model = model.fit(iris.data, iris.target)
+    X = iris.data[:, :2]
+    y = iris.target
 
-print(iris.data)
-print(iris.target)
-print(iris.target_names)
+    model = model.fit(X, y)
+    return model
 
-model = KNeighborsClassifier(n_neighbors=3)
-# model = model.fit(iris.data, iris.target)
-X = iris.data[:, :2];
-y = iris.target;
+def plotKNearestNeighbors(iris, model):
+    h = .02  # step size in the mesh
 
-model = model.fit(X, y)
+    X = iris.data[:, :2]
+    y = iris.target
 
-h = .02  # step size in the mesh
+    # Create color maps
+    cmap_light = ListedColormap(['orange', 'cyan', 'cornflowerblue'])
+    cmap_bold = ListedColormap(['darkorange', 'c', 'darkblue'])
 
-# Create color maps
-cmap_light = ListedColormap(['orange', 'cyan', 'cornflowerblue'])
-cmap_bold = ListedColormap(['darkorange', 'c', 'darkblue'])
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
 
-# Plot the decision boundary. For that, we will assign a color to each
-# point in the mesh [x_min, x_max]x[y_min, y_max].
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
 
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-plt.figure()
-plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title("3-Class classification (k = %i, weights = '%s')" % (3, 0))
 
-# Plot also the training points
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
-plt.xlim(xx.min(), xx.max())
-plt.ylim(yy.min(), yy.max())
-plt.title("3-Class classification (k = %i, weights = '%s')" % (3, 0))
-
-plt.show()
+    plt.show()
