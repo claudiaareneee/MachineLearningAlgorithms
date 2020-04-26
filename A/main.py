f4cbec58b086b22c38da4856e85a46fc4e46a8f0
Claudia <claudiaareneee@gmail.com>
2020-04-26 12:56:11 -0500

    moved output to a file

--- A/main.py
+++ A/main.py
@@ -12,10 +12,13 @@ import numpy as np
 iris = datasets.load_iris()
 X_train, X_test, y_train, y_test = model_selection.train_test_split(iris.data, iris.target, test_size = 0.20)
 
+global file
+
 def makeTestPrediction(model, iris):
     value = [5, 3.4, 1.6, 0.4]
     result = model.predict([ value,])# What is the iris class for 3cm x 5cm sepal and 4cm x 2cm petal?
-    print ("%dcm x %dcm sepal and %dcm x %dcm petal: " %tuple(value) + str(iris.target_names[result]))
+    file.write ("%dcm x %dcm sepal and %dcm x %dcm petal: " %tuple(value) + str(iris.target_names[result]))
+    file.write("\n")
 
 def plotData(x, y, title):
     formatter = plt.FuncFormatter(lambda i, *args: iris.target_names[int(i)])
@@ -81,35 +84,39 @@ def spotCheck(name, model):
 
     predictions = model.predict(X_test)
     score = accuracy_score(y_test, predictions)
-    print("Test accuracy score: %f" % score)
-    print("Confusion matrix:")
+    file.write("Test accuracy score: %f\n" % score)
+    file.write("Confusion matrix:\n")
     score = confusion_matrix(y_test, predictions)
-    print(score)
-    print("Classification report:")
+    file.write(str(score))
+    file.write("\n")
+    file.write("Classification report:\n")
     score  = classification_report(y_test, predictions)
-    print(score)
+    file.write(score)
+    file.write("\n")
 
     kfold = model_selection.KFold(n_splits=10)
     cv_results = model_selection.cross_val_score(model, X_train, y_train, cv=kfold, scoring='accuracy')
-    msg = "Training accuracy score: %f (%f)" % (cv_results.mean(), cv_results.std())
-    print(msg)
+    msg = "Training accuracy score: %f (%f)\n" % (cv_results.mean(), cv_results.std())
+    file.write(msg)
 
 if __name__ == "__main__":
-    # print("Iris data:")
-    # print(iris.data)
-    # print("Iris target:")
-    # print(iris.target)
-    # print("Iris target names:")
-    # print(iris.target_names)
+    # file.write("Iris data:")
+    # file.write(iris.data)
+    # file.write("Iris target:")
+    # file.write(iris.target)
+    # file.write("Iris target names:")
+    # file.write(iris.target_names)
 
-    # print("X_train:")
-    # print(X_train)
-    # print("X_test:")
-    # print(X_test)
-    # print("y_train:")
-    # print(y_train)
-    # print("y_test:")
-    # print(y_test)
+    # file.write("X_train:")
+    # file.write(X_train)
+    # file.write("X_test:")
+    # file.write(X_test)
+    # file.write("y_train:")
+    # file.write(y_train)
+    # file.write("y_test:")
+    # file.write(y_test)
+
+    file = open('metrix.txt', 'w')
 
     plotData(0,1,"Iris classification according to Sepal measurements")
     plotData(2,3,"Iris classification according to Petal measurements")
@@ -125,9 +132,9 @@ if __name__ == "__main__":
     # dtree.plotDecisionTree(model)
 
     for name, model in models:
-        print("---------------------------------------------------------------------------------------------")
-        print(name)
-        print("")
+        file.write("---------------------------------------------------------------------------------------------\n")
+        file.write(name)
+        file.write("\n")
 
         supervised = True
         if (name is "Decision Tree"):
@@ -138,5 +145,7 @@ if __name__ == "__main__":
         makeTestPrediction(model, iris)
         spotCheck(name, model)
         plotAll(model, iris, X_train, X_test, y_train, y_test, name, plotSepal=False, supervised=supervised)
+    
+    file.close()
 
 # TODO: k Means Clustering
